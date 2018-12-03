9bba82e8b85356fa96edcd124dca04a3b7d5dc8c
Ubuntu <xikong@MyAzureVMServer.ct4rhpmvaesenfme3jsbgnn15h.ix.internal.cloudapp.net>
2018-12-03 09:45:42 +0000

    configuring static files

--- helloDjango/views.py
+++ helloDjango/views.py
@@ -4,8 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
+from django.contrib import staticfiles 
 def index(request):
-    return render(request,'home.html')
+    return render(request,'index.html')
 
 def stable(request):
     return render(request,'home.html')
