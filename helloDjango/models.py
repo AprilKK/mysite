15a4fb66a9bf06cd100c9f367f0a417069ab43e9
Xiaosong Kong <xikong@microsoft.com>
2018-12-04 17:19:14 +0800

    add blod DB(model)

--- helloDjango/models.py
+++ helloDjango/models.py
@@ -2,5 +2,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Artical(models.Model):
+    articalId = models.AutoField(primary_key = True) # auto incremental for the primary key.
+    #you use primary_key = True if you do not want to use default field "id" given by django to your model
+    title = models.CharField(max_length = 100)
+    subtitle = models.CharField(max_length = 500)
+    content = models.TextField()
+    submittedDate = models.DateField(default = date.today)
+    lastUpdatedDate = models.DateField(default = date.today)
+    author = models.CharField(default = 'xikong', max_length = 50)
+
+# use __str__ for python 3
+def __unicode__(self):
+    return self.title
+
+class Comment(models.Model):
+    commentId = models.AutoField(primary_key = True)
+    articalId = models.ForeignKey(Artical)
+    comment = models.TextField()
+    commentDate = models.DateField(default = date.today)
+    author = models.CharField(max_length = 50)
+
+# use __str__ for python 3
+def __unicode__(self):
+    return self.comment[:10]
+
