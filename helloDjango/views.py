55f2063338eba9e8db94eeb9b878067e868711ff
Ubuntu <xikong@MyAzureVMServer.ct4rhpmvaesenfme3jsbgnn15h.ix.internal.cloudapp.net>
2018-12-07 01:44:41 +0000

    markdown rendering can work now

--- helloDjango/views.py
+++ helloDjango/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
 from django.contrib import staticfiles 
 from helloDjango.models import Artical
+import markdown
 def index(request):
     return render(request,'index.html')
 
@@ -28,8 +29,13 @@ def blog(request):
     return render(request,'blog.html',{'artlist':artlist})
 
 def artical(request):
-   # with open('/static/articals/art1.md') as f:
-   #     content = f.read()
-    content = '##hed \n### head3'
-    return render(request,'artical.html',{'content':content})
+    with open('./helloDjango/articals/Readme.md') as f:
+        content = f.read()
+    contentutf = unicode(content,'utf-8')
+    art = markdown.markdown(contentutf,extensions = [
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+        ])
+    return render(request,'artical.html',{'content':art})
     
