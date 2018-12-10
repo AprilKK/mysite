f202bd0ca14395e7a5ea3a3ab3d427043dbcead7
Xiaosong Kong <xikong@microsoft.com>
2018-12-10 14:03:02 +0800

    fix next page not work issue

--- helloDjango/views.py
+++ helloDjango/views.py
@@ -15,16 +15,15 @@ def stable(request):
     return render(request,'home.html')
 
 def blog(request):
-    currentPage = 1
     page = request.GET.get('page')
     if page is not None:
         page = int(page)
+        currentPage = page
     else:
         page = 1
+        currentPage = 1
     if -1 == page:
        currentPage = currentPage +1 
-    else:
-        currentPage = page
     artlist = Artical.objects.all()[(currentPage-1)*3:currentPage*3]
     return render(request,'blog.html',{'artlist':artlist})
 
